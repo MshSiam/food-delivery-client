3fa6ef9745d5dc4fcde0100e5b9de90b7801de04
MshSiam <siyamkenway@gmail.com>
2022-08-08 17:45:44 +0600

    cart item

--- src/components/RowContainer.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const RowContainer = () => {
-  return <div className="w-full ">RowContainer</div>
-}
-
-export default RowContainer
--- src/context/initialState.js
+++ src/context/initialState.js
@@ -1,8 +1,11 @@
-import { fetchUser } from "../utilities/fetchLocalStorage"
+import { fetchCart, fetchUser } from "../utilities/fetchLocalStorage"
 
 const userInfo = fetchUser()
+const cartInfo = fetchCart()
 
 export const initialState = {
   user: userInfo,
-  foodItems: null
+  foodItems: null,
+  cartShow: false,
+  cartItems: cartInfo
 }
